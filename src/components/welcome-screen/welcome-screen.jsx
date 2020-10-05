3f644b6ef5444c0163b5172949e39b51a51d4569
unknown <kraynevroman@gmail.com>
2020-10-06 00:25:07 +0300

    add props validation

--- src/components/welcome-screen/welcome-screen.jsx
+++ src/components/welcome-screen/welcome-screen.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const WelcomeScreen = (props) => {
   // eslint-disable-next-line react/prop-types
@@ -21,6 +22,10 @@ const WelcomeScreen = (props) => {
       <p className="welcome__text">Удачи!</p>
     </section>
   );
+};
+
+WelcomeScreen.propTypes = {
+  errorsCount: PropTypes.number.isRequired,
 };
 
 export default WelcomeScreen;
